116fb81e2b07c285a26b16fb5f8fb0a8ee0da601
Alex Mgw <egjuice99@gmail.com>
2024-01-19 18:22:59 +0200

    fix

--- scripts/api.js
+++ scripts/api.js
@@ -1,146 +1,156 @@
-const { gotScraping } = require("got-scraping");
+const gotScraping = require("got-scraping");
 
-const API_URL = "https://block-explorer-api.mainnet.zksync.io/api";
-const MODULE_ACCOUNT = "account";
-const ACTION_BALANCE = "balance";
-const ACTION_TRANSACTIONS = "txlist";
-const ACTION_CONTRACT_SOURCE_CODE = "getsourcecode";
+const baseApiUrl = "https://block-explorer-api.mainnet.zksync.io/api";
 
-async function makeApiCall(endpoint, params, userAgent) {
-  const response = await gotScraping.get(API_URL, {
-    searchParams: {
-      action: endpoint,
-      module: MODULE_ACCOUNT,
-      ...params,
-    },
-    headers: {
-      "User-Agent": userAgent,
-    },
-  });
+const walletAddress = "0x4F471D378B84422A971846e85bE3792b7f0f63EA";
 
-  const result = JSON.parse(response.body);
+const txListApiEndpoint = "module=account&action=txlist";
+const balanceApiEndpoint = "module=account&action=balance";
+const contractApiEndpoint = "module=contract&action=getsourcecode";
 
-  if ((result.body = "1")) {
-    return result.result;
-  } else {
-    throw new Error("Api error: ", result.message);
-  }
+function weiToEth(wei) {
+  return wei / 1e18;
 }
 
-async function getWalletBalance(walletAddress, userAgent) {
-  const params = { address: walletAddress };
-  return makeApiCall(ACTION_BALANCE, params, userAgent);
+function buildApiUrl(apiEndpoint, params) {
+  return `${baseApiUrl}?${apiEndpoint}&${new URLSearchParams(params)}`;
 }
 
-async function getWalletTransactions(walletAddress, userAgent) {
-  const params = { address: walletAddress };
-  let allTransactions = [];
+async function makeApiRequest(apiUrl) {
+  try {
+    const response = await gotScraping.got(apiUrl, { responseType: "json" });
 
-  let page = 1;
-  while (true) {
-    try {
-      const result = await makeApiCall(
-        ACTION_TRANSACTIONS,
-        { ...params, page },
-        userAgent
-      );
-
-      let succesfulTransactions = result.filter((tx) => tx.isError === "0");
-      allTransactions = allTransactions.concat(succesfulTransactions);
-
-      if (result.length === 0) {
-        break;
-      }
-
-      page++;
-    } catch (error) {
-      throw new Error(error.message);
+    if (response.body.status === "1") {
+      return response.body.result;
+    } else {
+      console.error("Error in API response:", response.body.message);
+      return null;
     }
-  }
-
-  return allTransactions;
-}
-
-async function getUniqueAddresses(walletAddress, userAgent) {
-  const params = { address: walletAddress.toLowerCase() };
-
-  let uniqueAddresses = new Set();
-
-  let page = 1;
-  while (true) {
-    try {
-      const result = await makeApiCall(
-        ACTION_TRANSACTIONS,
-        { ...params, page },
-        userAgent
-      );
-
-      if (result.length === 0) {
-        break;
-      }
-
-      result.forEach((tx) => {
-        const toAddress = tx.to.toLowerCase();
-        const fromAddress = tx.from.toLowerCase();
-
-        if (toAddress !== params.address) {
-          uniqueAddresses.add(toAddress);
-        }
-
-        if (fromAddress !== params.address) {
-          uniqueAddresses.add(fromAddress);
-        }
-      });
-
-      page++;
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-
-  return uniqueAddresses;
-}
-
-async function getLastInteractionDateTime(walletAddress, userAgent) {
-  const params = { address: walletAddress };
-  let lastInteractionDateTime = null;
-
-  let page = 1;
-  while (true) {
-    try {
-      const result = await makeApiCall(
-        ACTION_TRANSACTIONS,
-        { ...params, page },
-        userAgent
-      );
-
-      if (result.length === 0) {
-        break;
-      }
-
-      result.forEach((tx) => {
-        const timestamp = parseInt(tx.timeStamp, 10) * 1000;
-        if (!lastInteractionDateTime || timestamp > lastInteractionDateTime) {
-          lastInteractionDateTime = timestamp;
-        }
-      });
-
-      page++;
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-
-  if (lastInteractionDateTime) {
-    return lastInteractionDateTime;
-  } else {
+  } catch (error) {
+    console.error("Error making API request:", error.message);
     return null;
   }
 }
 
-module.exports = {
-  getWalletBalance,
-  getWalletTransactions,
-  getUniqueAddresses,
-  getLastInteractionDateTime,
-};
+async function walletBalance(apiEndpoint, params = {}) {
+  try {
+    const apiUrl = buildApiUrl(apiEndpoint, params);
+    const dataInWei = await makeApiRequest(apiUrl);
+
+    if (dataInWei !== null) {
+      const dataInEth = weiToEth(dataInWei);
+      return dataInEth;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error("Error in balance function:", error.message);
+    return null;
+  }
+}
+
+async function getTransactions(walletAddress) {
+  const pageSize = 10; // Number of transactions per page
+  let page = 1;
+  let allTransactions = [];
+
+  try {
+    while (true) {
+      const txListParams = { address: walletAddress, page, offset: pageSize };
+      const apiUrl = buildApiUrl(txListApiEndpoint, txListParams);
+      const response = await makeApiRequest(apiUrl);
+
+      if (response !== null && Array.isArray(response)) {
+        const filteredTransactions = response.filter(
+          (transaction) => transaction.isError === "0"
+        );
+
+        allTransactions = allTransactions.concat(filteredTransactions);
+
+        if (response.length < pageSize) {
+          break;
+        } else {
+          page++;
+        }
+      } else {
+        console.error("Error retrieving transactions:", response);
+        return null;
+      }
+    }
+
+    return allTransactions;
+  } catch (error) {
+    console.error("Error in getTransactions function:", error.message);
+    return null;
+  }
+}
+
+async function getUniqueAddresses() {
+  const transactions = await getTransactions(walletAddress);
+
+  if (transactions !== null) {
+    let uniqueAddresses = new Set();
+
+    transactions.forEach((transaction) => {
+      uniqueAddresses.add(transaction.to);
+      uniqueAddresses.add(transaction.from);
+    });
+    uniqueAddresses = new Set(
+      [...uniqueAddresses].filter((address) => address !== walletAddress)
+    );
+
+    return uniqueAddresses;
+  }
+}
+
+async function timestampForLastTransaction(walletAddress) {
+  try {
+    const transactions = await getTransactions(walletAddress);
+
+    if (transactions && transactions.length > 0) {
+      const sortedTransactions = transactions.sort(
+        (a, b) => b.blockNumber - a.blockNumber
+      );
+
+      const lastTransactionTimestamp = sortedTransactions[0].timeStamp;
+
+      return lastTransactionTimestamp;
+    } else {
+      console.error("No transactions found for the address.");
+      return null;
+    }
+  } catch (error) {
+    console.error(
+      "Error in timestampForLastTransaction function:",
+      error.message
+    );
+    return null;
+  }
+}
+
+async function main() {
+  try {
+    const balanceParams = { address: walletAddress };
+    const balanceInEth = await walletBalance(balanceApiEndpoint, balanceParams);
+
+    const transactionCount = await getTransactions(walletAddress);
+
+    const uniqueAddresses = await getUniqueAddresses();
+
+    const timestamp = await timestampForLastTransaction(walletAddress);
+
+    const walletStats = {
+      walletAddress: walletAddress,
+      walletBalance: balanceInEth,
+      transactionCount: transactionCount.length,
+      uniqueAddressesCount: [...uniqueAddresses].length,
+      uniqueAddresses: uniqueAddresses,
+      timestamp: timestamp,
+    };
+    console.log("Wallet Stats:", walletStats);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+main();
